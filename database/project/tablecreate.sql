fac9348cfacbae53f1719eaab590cf9e800e7146
Navarurh Kumar <navarurh@gmail.com>
2018-12-11 04:23:31 -0600

    db project completion mid
    finished till creation of tables and erd pending fixing a few issues here and there

---
+++ database/project/tablecreate.sql
@@ -0,0 +1,147 @@
+CREATE DATABASE project;
+USE project;
+
+CREATE TABLE IF NOT EXISTS brand
+(
+    brand_id    bigint(4)   not null,
+    brand_name  text(100),
+    brand_type  text(20),
+    PRIMARY KEY (brand_id)
+);
+
+CREATE TABLE IF NOT EXISTS  customer
+(
+    cust_code       bigint(4)   not null,
+    cust_fname      text(20),
+    cust_lname      text(20),
+    cust_street     text(70),
+    cust_city       text(50),
+    cust_state      text(2),
+    cust_zip        text(5),
+    cust_balance    decimal(16) not null,
+    PRIMARY KEY (cust_code)
+);
+
+CREATE TABLE IF NOT EXISTS  department
+(
+    dept_num        bigint(4)   not null,
+    dept_name       text(50),
+    dept_mail_box   text(3),
+    dept_phone      text(9),
+    supv_emp_num    bigint(4)   not null,
+    PRIMARY KEY (dept_num)
+);
+
+CREATE TABLE IF NOT EXISTS  employee
+(
+    emp_num         bigint(4)   not null,
+    emp_fname       text(20),
+    emp_lname       text(20),
+    emp_email       text(25),
+    emp_phone       text(20),
+    emp_hiredate    datetime not null,
+    emp_title       text(45),
+    emp_comm        decimal(16) not null,
+    dept_num        bigint(4)   not null,
+    PRIMARY KEY (emp_num),
+    INDEX   idx_dn(dept_num asc),
+    INDEX   idx_en(emp_num asc)
+);
+
+CREATE TABLE IF NOT EXISTS  invoice
+(
+    inv_num     bigint(4)   not null,
+    inv_date    datetime not null,
+    cust_code   bigint(4)   not null,
+    inv_total   decimal(16) not null,
+    emp_num     bigint(4)   not null,
+    PRIMARY KEY (inv_num),
+    INDEX   idx_cc(cust_code asc),
+    INDEX   idx_in(inv_num asc),
+    INDEX   idx_en(emp_num asc)
+);
+
+CREATE TABLE IF NOT EXISTS  line
+(
+    inv_num     bigint(4)   not null,
+    line_num    bigint(4)   not null,
+    prod_sku    text(15),
+    line_qty    bigint(8)   not null,
+    line_price  decimal(16) not null,
+    PRIMARY KEY (line_num)
+);
+
+CREATE TABLE IF NOT EXISTS  product
+(
+    prod_sku        text(15),
+    prod_descript   text(255),
+    prod_type       text(255),
+    prod_base       text(255),
+    prod_category   text(255),
+    prod_price      decimal(16) not null,
+    prod_qoh        decimal(16) not null,
+    prod_min        decimal(16) not null,
+    brand_id        bigint(4)   not null
+--    PRIMARY KEY (line_num),
+);
+
+CREATE TABLE IF NOT EXISTS  salary_history
+(
+    emp_num     bigint(4)   not null,
+    sal_from    datetime not null,
+    sal_end     datetime not null,
+    sal_amount  decimal(16) not null
+);
+
+CREATE TABLE IF NOT EXISTS  supplies
+(
+    prod_sku    text(15),
+    vend_id     bigint(4)   not null
+);
+
+CREATE TABLE IF NOT EXISTS  vendor
+(
+    vend_id     bigint(4)   not null,
+    vend_name   text(255),
+    vend_street text(50),
+    vend_city   text(50),
+    vend_state  text(2),
+    vend_zip    text(5),
+    PRIMARY KEY (vend_id)
+);
+
+
+ALTER TABLE department
+ADD FOREIGN KEY (supv_emp_num) REFERENCES employee(emp_num);
+
+ALTER TABLE employee
+ADD FOREIGN KEY (dept_num) REFERENCES department(dept_num);
+
+ALTER TABLE invoice
+ADD FOREIGN KEY (cust_code) REFERENCES customer(cust_code),
+ADD FOREIGN KEY (emp_num) REFERENCES employee(emp_num);
+
+ALTER TABLE line
+ADD FOREIGN KEY (inv_num) REFERENCES invoice(inv_num);
+
+ALTER TABLE product
+ADD FOREIGN KEY (brand_id) REFERENCES brand(brand_id);
+
+ALTER TABLE salary_history
+ADD FOREIGN KEY (emp_num) REFERENCES employee(emp_num);
+
+ALTER TABLE supplies
+ADD FOREIGN KEY (vend_id) REFERENCES vendor(vend_id);
+
+
+
+select count(*) from brand;
+select count(*) from customer;
+select count(*) from department;
+select count(*) from employee;
+select count(*) from invoice;
+select count(*) from line;
+select count(*) from product;
+select count(*) from salary_history;
+select count(*) from supplies;
+select count(*) from vendor;
--- database/project/test_data.R
+++ database/project/test_data.R
@@ -1,6 +1,59 @@
 setwd("~/documents/assignments/database/project/")
+
+library(plyr)
+library(RMySQL)
+
 d1 <- read.csv("datasets/BUAN6320-DataSet1.txt",header = T,stringsAsFactors = F,sep = "\t")
 d2 <- read.csv("datasets/BUAN6320-DataSet2.txt",header = T,stringsAsFactors = F,sep = "\t")
 d3 <- read.csv("datasets/BUAN6320-DataSet3_pytmp.txt",header = T,stringsAsFactors = F,sep = "\t",fileEncoding="utf-8")
 d4 <- read.csv("datasets/BUAN6320-DataSet4.txt",header = T,stringsAsFactors = F,sep = "\t")
 
+colnames(d1)
+colnames(d2)
+colnames(d3)
+colnames(d4)
+
+brand <- d3[c(12,2,9)]
+brand <- unique(brand)
+customer <- d1[c(2,3,5,6,8,10,12,14)]
+customer <- unique(customer)
+department <- d4[c(12,13,14,15,16)]
+department <- unique(department)
+employee <- d4[c(1,2,3,4,5,6,7,8,12)]
+employee <- unique(employee)
+invoice <- d1[c(13,16,2,9,15)]
+invoice <- unique(invoice)
+line <- d1[c(13,11,7,4,1)]
+line <- unique(line)
+product <- d3[c(1,4,5,17,7,11,14,16,12)]
+product <- unique(product)
+salary_history <- d4[c(1,9,10,11)]
+salary_history <- unique(salary_history)
+supplies <- d3[c(1,13)]
+supplies <- unique(supplies)
+vendor <- d3[c(13,6,3,10,8,15)]
+vendor <- unique(vendor)
+
+invoice <- invoice[!duplicated(invoice$INV_NUM),]
+vendor <- vendor[which(vendor$VEND_NAME != ""),]
+colnames(invoice)[5] <- "EMP_NUM"
+
+employee$EMP_HIREDATE <- as.POSIXct(employee$EMP_HIREDATE, origin = "1900-01-01")
+salary_history$SAL_FROM <- as.POSIXct(salary_history$SAL_FROM, origin = "1900-01-01")
+salary_history$SAL_END[which(salary_history$SAL_END == " - ")] <- 0
+salary_history$SAL_END <- as.integer(salary_history$SAL_END)
+salary_history$SAL_END <- as.POSIXct(salary_history$SAL_END, origin = "1900-01-01")
+invoice$INV_DATE <- as.POSIXct(invoice$INV_DATE, origin = "1900-01-01")
+
+con <- dbConnect(MySQL(), user='root', password='n', dbname='project', host='localhost')
+
+dbWriteTable(con, name='brand', brand, append=T, row.names = F)
+dbWriteTable(con, name='customer', customer, append=T, row.names = F)
+dbWriteTable(con, name='department', department, append=T, row.names = F)
+dbWriteTable(con, name='employee', employee, append=T, row.names = F)
+dbWriteTable(con, name='invoice', invoice, append=T, row.names = F)
+dbWriteTable(con, name='line', line, append=T, row.names = F)
+dbWriteTable(con, name='product', product, append=T, row.names = F)
+dbWriteTable(con, name='salary_history', salary_history, append=T, row.names = F)
+dbWriteTable(con, name='supplies', supplies, append=T, row.names = F)
+dbWriteTable(con, name='vendor', vendor, append=T, row.names = F)
